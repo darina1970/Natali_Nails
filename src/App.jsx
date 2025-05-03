1a27b3739546ee426c53eb31b97675d9c7542a59
Dasha <darina1970@mail.ru>
2025-05-04 03:24:22 +0700

    add images, icon, meta-tags, fonts

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// Импорт компонентов
+// import Header from './components/Header/Header';
+// import Banner from './components/Banner/Banner';
+// import AboutMe from './components/About/About';
+// import WhyChooseMe from './components/WhyChoose/WhyChoose';
+// import Portfolio from './components/Portfolio/Portfolio';
+// import Price from './components/Price/Price';
+// import Sterilization from './components/Sterilization/Sterilization';
+// import Certificate from './components/Certificate/Certificate';
+// import Contacts from './components/Contacts/Contacts';
+// import Footer from './components/Footer/Footer';
+
+function App() {
+    return (
+        <div className="App">
+            {/* <Header />
+            <Banner />
+            <AboutMe />
+            <WhyChooseMe />
+            <Portfolio />
+            <Price />
+            <Sterilization />
+            <Certificate />
+            <Contacts />
+            <Footer /> */}
+        </div>
+    );
+}
+
+export default App;

d1074596c9c9ba19d6e37840aeb00b936ace77c3
Dasha <darina1970@mail.ru>
2025-05-05 11:30:29 +0700

    create about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from './About.module.css';
+import aboutImage from '../../assets/images/about-me-img.webp';
+
+
+
+export const About = () => {
+    return (
+        <section>
+            <div className="container">
+                <h2>Обо мне</h2>
+                <div className={styles.contentWrapper}>
+                    <div className={styles.imageWrapper}>
+                        <img src={aboutImage} className={styles.aboutImage} alt="Nail master at work" />
+                    </div>
+                    <ul className={styles.benefitsList}>
+                        <li>Я обеспечиваю эстетику ногтей, совмещая безопасность, качество и доступность.</li>
+                        <li>В работе применяю стерильные инструменты и проверенные материалы премиум-класса.</li>
+                        <li>Для каждого клиента разрабатываю индивидуальный подход, подбирая процедуры маникюра и педикюра с учетом состояния ногтей и пожеланий.</li>
+                        <li>Моя цель — идеальный результат без компромиссов в уходе и здоровье.</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )    
+} 

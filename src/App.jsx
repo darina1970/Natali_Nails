58dbed12bc48dbe96b2ad5cd3d4d74c2b79debdc
Ninima1218 <ninima1218@gmail.com>
2025-05-09 22:23:45 +0400

    adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import React from "react";
 // import Banner from './components/Banner/Banner';
 import { About } from "./components/About/About";
 import { WhyChoose } from "./components/WhyChoose/WhyChoose";
-// import Portfolio from './components/Portfolio/Portfolio';
-// import Price from './components/Price/Price';
+import { Portfolio } from "./components/Portfolio/Portfolio";
+import { Price } from "./components/Price/Price";
 import { Sterilization } from "./components/Sterilization/Sterilization";
 import { Certificate } from "./components/Certificate/Certificate";
 import { Contacts } from "./components/Contacts/Contacts";
@@ -15,12 +15,12 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-            <Banner /> */}
+      {/* <Header /> */}
+      {/* <Banner /> */}
       <About />
       <WhyChoose />
-      {/* <Portfolio />
-            <Price /> */}
+      <Portfolio />
+      <Price />
       <Sterilization />
       <Certificate />
       <Contacts />
